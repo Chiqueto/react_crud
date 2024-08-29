aaf553452c6aa8c7ad966b2fa8175f7f14090c07
chiqueto <lfchiqueto@gmail.com>
2024-08-29 14:07:45 -0300

    Commit

---
+++ src/services/api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3001'
+});
+
+export const getMovies = () => api.get('/movies');
+export const getMoviesById = (id: string) => api.get(`/movies/${id}`);
+export const createMovie = (movie: any) => api.post('/movies', movie);
+export const updateMovie = (id: string, movie: any) => api.put(`/movies/${id}`, movie);
+export const deleteMovie = (id: string) => api.delete(`/movie/${id}`);
